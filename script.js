651e583bf1501248dfaf5a7cbb0f73f8996d87b9
Evasive <albinshiju285@gmail.com>
2025-03-20 09:35:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+const crypto = require('crypto');
+
 const encrypt = (payload) => {
-  // encrypt the payload and return token
-}
+  const cipher = crypto.createCipheriv('aes-256-cbc', secretKey, iv);
+  let encrypted = cipher.update(JSON.stringify(payload), 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return { token: encrypted, iv: iv.toString('hex') };
+};
 
-const decrypt = (token) => {
-  // return decoded payload
-}
-
-module.exports = {
-  encrypt,
-  decrypt
-}
+const decrypt = (encryptedData) => {
+  const decipher = crypto.createDecipheriv('aes-256-cbc', secretKey, Buffer.from(encryptedData.iv, 'hex'));
+  let decrypted = decipher.update(encryptedData.token, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return JSON.parse(decrypted);
+};
